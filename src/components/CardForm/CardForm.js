cd2354fba9308bf3d45168ff676d11c4c53e19ac
Maks Egorov <emax@makeit-da.ru>
2018-09-21 18:28:32 +0300

    completed CardForm

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import Title from 'components/Title/Title';
 
 import './CardForm.css';
 
 class CardForm extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className=""></div>
-         );
-    }
+  handleChangeForm = e => {
+    this.props.onChangeForm(e.target.name, e.target.value);
+  };
+  render() {
+    return (
+      <div className="card-form">
+        <Title title={'Номер карты'} />
+        <input
+          type="text"
+          name="cardNumber"
+          placeholder="0000000000000000"
+          onChange={this.handleChangeForm}
+          value={this.props.cardNumber}
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default CardForm;
