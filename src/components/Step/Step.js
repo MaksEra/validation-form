aa2b986e34fa0bf9bae5380e966ae3170a6dd9eb
Maks Egorov <emax@makeit-da.ru>
2018-09-21 16:11:25 +0300

    cc Step

--- src/components/Step/Step.js
+++ src/components/Step/Step.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import './Step.css';
+
+class Step extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className='step'></div>
+         );
+    }
+}
+ 
+export default Step;
