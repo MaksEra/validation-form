044ff29dcf4e52157bf37b9c2fb9b924981c9b81
Maks Egorov <emax@makeit-da.ru>
2018-09-21 16:16:38 +0300

    create CardForm.js

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './CardForm.css';
+
+class CardForm extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className=""></div>
+         );
+    }
+}
+ 
+export default CardForm;
