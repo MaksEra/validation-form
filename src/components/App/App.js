2c43bd841a493064477a0eea140a1a5ca31c6de9
Maks Egorov <emax@makeit-da.ru>
2018-09-21 16:08:05 +0300

    create project and add App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className='container'>
+                <div className='tab-panel'>
+                    
+                </div>
+            </div> 
+        );
+    }
+}
+ 
+export default App;
