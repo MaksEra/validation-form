21fb443f2b4ce1ac3543d9df75e9fc898454481b
Maks Egorov <emax@makeit-da.ru>
2018-09-21 17:00:33 +0300

    create render Steps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import Step from 'components/Step/Step';
+import CardForm from 'components/CardForm/CardForm';
+import PersonalForm from 'components/PersonalForm/PersonalForm';
 
 import './App.css';
 
-
 class App extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className='container'>
-                <div className='tab-panel'>
-                    <Step />
-                </div>
-            </div> 
-        );
-    }
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    cardNumber: ''
+  };
+
+  render() {
+    const tabs = [
+      { number: 1, title: 'Personal information' },
+      { number: 2, title: 'Card information' },
+      { number: 3, title: 'Finish' }
+    ];
+
+    return (
+      <div className="container">
+        <div className="tab-panel">
+          {tabs.map(tab => {
+            const { number, title } = tab;
+            return (
+              <Step key={number} number={number}>
+                {title}
+              </Step>
+            );
+          })}
+        </div>
+        <div className="form-content" />
+        <div className="button-panel">
+          <button className="button-next" onClick="">
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default App;
--- src/components/Step/Step.js
+++ src/components/Step/Step.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './Step.css';
 
 class Step extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className='step'></div>
-         );
+  handleClick = () => {
+    let number = this.props.number;
+    if (this.props.isClickable) {
+      this.props.onClick(number);
     }
+  };
+
+  render() {
+    const { isClickable, isSelected, number } = this.props;
+    return (
+      <div
+        className={`step ${(isSelected && 'step-selected') ||
+          ''} ${(isClickable && 'step-clickable') || ''}`}
+        value={number}
+        onclick={this.handleClick}
+      >
+        <p className="step__number">{number}</p>
+        <p className="step__title">{this.props.children}</p>
+      </div>
+    );
+  }
 }
- 
+
 export default Step;
